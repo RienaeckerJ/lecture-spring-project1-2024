6bba8ca2174f9009a85b91955da32be6808978fa
Jörg Rienäcker <e12129856@student.tuwien.ac.at>
2024-03-26 21:16:11 +0100

    update test function bc of error

--- test_utils.py
+++ test_utils.py
@@ -6,6 +6,6 @@ def test_add():
     assert add(3,4) == 7
 
 def test_substract():
-    assert substract(x:2, y:1) == 1
-    assert substract(x:5, y:2) == 3
-    assert substract(x:6, y:3) == 3
+    assert substract(2, 1) == 1
+    assert substract(5, 2) == 3
+    assert substract(6, 3) == 3
