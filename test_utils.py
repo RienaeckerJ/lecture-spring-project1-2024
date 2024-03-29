718fae4f2391a23872791fca9824bbe59b0a92c6
Jörg Rienäcker <e12129856@student.tuwien.ac.at>
2024-03-29 12:01:44 +0100

    formating with ruff format

--- test_utils.py
+++ test_utils.py
@@ -1,9 +1,12 @@
 from utils import add
 from utils import substract
+
+
 def test_add():
     assert add(1, 2) == 3
     assert add(5, 5) == 10
     assert add(3, 4) == 7
+
 
 def test_substract():
     assert substract(2, 1) == 1
