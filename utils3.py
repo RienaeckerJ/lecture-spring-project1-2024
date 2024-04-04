d37eb4e13c98c7f1d6b2075d48eef9fad21a28ba
Jörg Rienäcker <e12129856@student.tuwien.ac.at>
2024-04-04 13:13:06 +0200

    Adding the functino sum_of_even_squares and formating

---
+++ utils3.py
@@ -0,0 +1,8 @@
+def sum_of_even_squares(
+    numbers,
+):  # definging a function which calculates the sum of the squares of the even numbers in the list
+    sum_of_squares = 0
+    for num in numbers:
+        if num % 2 == 0:
+            sum_of_squares += num**2
+    return sum_of_squares
