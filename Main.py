2aaef86d7e8e63fa94e5c20f4b54680643b29ac0
Jörg Rienäcker <e12129856@student.tuwien.ac.at>
2024-04-04 13:54:49 +0200

    Adding comments to main

--- Main.py
+++ Main.py
@@ -1,3 +1,3 @@
-from utils3 import sum_of_even_squares
+from utils3 import sum_of_even_squares  # importing the function sum_of_even_squares from the file utils3
 
-sum_of_even_squares([2, 4])
+sum_of_even_squares([2, 4])  #  calling the function, result is printed in the console
