c01af5104c45dcff7b003322722ebd27fa43bf73
tschmdt <e12112219@student.tuwien.ac.at>
2024-04-09 18:57:46 +0200

    Make ruff fail

--- test_utils.py
+++ test_utils.py
@@ -3,12 +3,10 @@ from utils import substract  # importing the function substract from the file ut
 
 
 def test_add():  # defining a test function to test the function add
-    assert add(1, 2) == 3
+    assert add(1, 2)==3
     assert add(5, 5) == 12
     assert add(3, 4) == 7
-
-
 def test_substract():  # defining a test function to test the function substract
-    assert substract(2, 1) == 1
+    assert substract(2, 1)==1
     assert substract(5, 2) == 4
     assert substract(6, 3) == 3
